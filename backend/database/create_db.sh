01f3a0d71a4ceb57d8365cd5509efed5d4b19850
Jeff Crosley <jeffcrosley@gmail.com>
2020-04-21 14:05:35 -0400

    fixed database bash script

--- backend/database/create_db.sh
+++ backend/database/create_db.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 BASEDIR=$(dirname $0)
-DATABASE=userdb
+DATABASE=stockwars
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/user.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/game.sql"
+psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql"
