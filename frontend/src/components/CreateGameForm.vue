c07694ddfe0c2022f12f5339f38c04318aecf808
Jake Casassa <jakecasassa@gmail.com>
2020-04-21 13:43:48 -0400

    starting creategame component

---
+++ frontend/src/components/CreateGameForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="new-game">
+        <ul>
+            <!-- <div v-for=> -->
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            gameId: '',
+            name: '',
+            numberOfPlayer: '',
+            lengthInDays: '',
+            startDate: '',
+            endDate: '',
+            publicGame: false,
+            organiserId: '',
+            winnerId: ''
+        }
+    },
+    methods: {
+        createGame() {
+      
+
+        }
+    }
+}
+</script>
