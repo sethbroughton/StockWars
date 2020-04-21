a74d5363293e85bdff553be471eb55b610a206ca
Charles Ireland <chuckireland1@gmail.com>
2020-04-21 14:22:36 -0400

    GameController

---
+++ backend/src/main/java/com/techelevator/controller/GameController.java
@@ -0,0 +1,13 @@
+package com.techelevator.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/game")
+public class GameController {
+	
+
+}
