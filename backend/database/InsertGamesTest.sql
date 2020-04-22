aa156c3566f4c83f816784580ce03b58986a92b0
Charles Ireland <chuckireland1@gmail.com>
2020-04-22 12:46:13 -0400

    Starting to insert data into tables for testing

--- backend/database/InsertGamesTest.sql
+++ backend/database/InsertGamesTest.sql
@@ -1,5 +1,8 @@
 BEGIN TRANSACTION;
 INSERT INTO game (organizer_id, name, number_of_players, length_in_days, start_date, end_date, public_game) VALUES (1, 'Langley', 3, 30, '1990-03-07', '1990-03-12', true);
+
+
+
 ROLLBACK;
 COMMIT;
 
