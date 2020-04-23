a392e8ae39f95bc15d88e0764105e33d35955b4c
Seth Broughton <broughton.24@gmail.com>
2020-04-23 13:16:46 -0400

    Added query to Search.vue - all working

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -2,6 +2,7 @@
   <div id="search">
     <user-header></user-header>
     <h1>Search</h1>
+    
     <p>{{data[0].name}}</p>
 
     <routes/>
@@ -16,8 +17,11 @@ export default {
   name: 'search',
     data() {
         return {
-          data: []
-
+          data: [{
+            name: ''
+          }],
+          query: 'AAPL',
+          apikey: 'a7f7aa92b83ad64df4f2f540028a9880'
         }
     },
   components: {
@@ -25,12 +29,17 @@ export default {
     UserHeader
   }, 
     created() {
-      fetch('https://fmpcloud.io/api/v3/quote/AAPL?apikey=a7f7aa92b83ad64df4f2f540028a9880')
+      const query = this.query
+      const apikey = this.apikey
+      console.log(apikey);
+      fetch(`https://fmpcloud.io/api/v3/quote/${query}?apikey=${apikey}`
+      )
         .then((response) => {
           return response.json();
         })
         .then((data) => {
           this.data = data
+          console.log('hi')
 
         });
     }
