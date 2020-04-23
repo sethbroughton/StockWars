f39d2b430458884ac63a281051982d9a023059f5
Seth Broughton <broughton.24@gmail.com>
2020-04-23 16:35:36 -0400

    Stock Search page working - need to build table of stocks

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -4,10 +4,10 @@
     <routes/>
 
 <h1>Search</h1>
- <form @submit.prevent="search" class="form u-margin-bottom">
+ <form @submit.prevent="searchStocks" class="form u-margin-bottom">
         <div class="input-fields">
           <div class="form-group u-margin-bottom-small">
-            <label for="search" class="label">Ticker Symbol</label>
+            <label for="search" class="label">Search by Ticker</label>
             <input
               type="text"
               id="search"
@@ -19,7 +19,27 @@
       <button type="submit" id="search" class="button">Search</button>
       </form>
 
+      <form @submit.prevent="findCompany" class="form u-margin-bottom">
+        <div class="input-fields">
+          <div class="form-group u-margin-bottom-small">
+            <label for="search" class="label">Find a Company</label>
+            <input
+              type="text"
+              id="search"
+              v-model="companyQuery"
+              autofocus
+            />
+          </div>
+        </div>
+      <button type="submit" id="search" class="button">Search</button>
+      </form>
+
+    
     <h3>{{data[0].name}}</h3>
+    {{data[0].price}}
+   
+
+{{bestMatches[0]['2. name']}}
 
 
   </div>
@@ -42,15 +62,16 @@ export default {
             price: ''
           }],
           query: '',
-          apikey: 'a7f7aa92b83ad64df4f2f540028a9880'
+          companyQuery: '',
+          obj: {},
+          bestMatches: []
         }
     },
   methods: {
-    search(){
+    searchStocks(){
       const query = this.query
       console.log(query);
-      const apikey = this.apikey
-      console.log(apikey);
+      const apikey = 'a7f7aa92b83ad64df4f2f540028a9880';
       fetch(`https://fmpcloud.io/api/v3/quote/${query}?apikey=${apikey}`)
         .then((response) => {
           return response.json();
@@ -58,7 +79,21 @@ export default {
         .then((data) => {
           this.data = data;
         })
-  }
+  }, 
+    findCompany(){
+      const companyQuery = this.companyQuery;
+      const apikey = 'H0HZ222NMZ40ICTV';
+    fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${companyQuery}&apikey=H0HZ222NMZ40ICTV`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((obj) => {
+          this.obj = obj;
+          this.bestMatches = obj.bestMatches;
+          console.log(this.bestMatches[0]['2. name'])
+        })
+
+    }
   },
   }
 </script>
