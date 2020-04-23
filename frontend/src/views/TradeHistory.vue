3549089b1bc44584db582f44191caa14854a6db7
Jake Casassa <jakecasassa@gmail.com>
2020-04-23 15:16:19 -0400

    html outlined for TradeHistory Vue

--- frontend/src/views/TradeHistory.vue
+++ frontend/src/views/TradeHistory.vue
@@ -2,8 +2,15 @@
   <div id="trade-history">
     <user-header></user-header>
     <h1>Trade History</h1>
-    <p>You must be authenticated to see this</p>
-    <routes/>
+    <div class="trade-history">
+      <button class="return-to-game-btn">Return New Game</button>
+      <ul class="trade-history-list">
+        <li>
+          (name of stock) - ticker symbol - buy or sell 
+          - num of shares - purchase price - date purchases
+        </li>       
+      </ul>
+    </div>
   </div>
 </template>
 
