a9ec4dd1dedc56bb05e6046c89f14bfca03238d4
Charles Ireland <chuckireland1@gmail.com>
2020-04-23 08:51:56 -0400

    Added Template for Portfolio Page

--- frontend/src/views/Portfolio.vue
+++ frontend/src/views/Portfolio.vue
@@ -1,6 +1,27 @@
 <template>
   <div id="portfolio">
-    <h1>Portfolio</h1>
+    <div class = "header">
+      <h3>Username</h3>
+        <li>Avatar</li>
+    </div>
+
+    <div class = "return-to-game-btn">
+        <button>Return to Game</button>
+    </div>
+
+    <div class = "portfolio-information">
+      <h1>Portfolio</h1>
+      <h1>Balance</h1>
+        <p class = "stock-information">
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </p>
+
+
     <p>You must be authenticated to see this</p>
     <routes/>
   </div>
@@ -13,6 +34,19 @@ export default {
   name: 'portfolio',
   components: {
     Routes
-  }
+  },
+  data() {
+    return {
+
+    }
+  };
+},
+methods: {
+
 }
+
 </script>
+
+<style scoped>
+
+</style>
