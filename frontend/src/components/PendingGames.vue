b4293f0b6af152045143f49a630af6c3ae675e88
Jeff Crosley <jeffcrosley@gmail.com>
2020-04-23 16:32:00 -0400

    basic styling and data binding in game lobby

---
+++ frontend/src/components/PendingGames.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="pending-games">
+    <h2>Pending Games</h2>
+    <ul class="pending-games-list">
+      <li v-for="game in pendingGames" :key="game.gameId">
+        {{game.name}} - Organizer: {{game.organizerId}}
+        - Players: ((# of players)/(max # of players)) -
+        {{game.lengthInDays}} Days
+        <button class="start-game-btn">Start Game</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import auth from '../auth'
+
+export default {
+  name: 'pending-games',
+  data() {
+    return {
+      pendingGames: []
+    };
+  },
+  created() {
+    const authToken = auth.getToken();
+    fetch(`${process.env.VUE_APP_REMOTE_API}/api/games`,{
+       method: 'GET',
+       headers:{
+        Authorization: `Bearer ${authToken}`
+        }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then((games) => {
+        this.pendingGames = games;
+      })
+      .catch(err => console.log(`Error fetching reviews ${err}`));
+  }
+
+}
+</script>
+
+<style>
+
+  #pending-games {
+    color: var(--color-grey-light-1);
+    background-color: var(--color-grey-dark-1);
+    margin: 3%;
+    padding: 2%;
+  }
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+</style>
