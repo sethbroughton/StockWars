21c1bac2b5224d18b867267a6882c06591fbe225
Seth Broughton <broughton.24@gmail.com>
2020-04-24 10:27:31 -0400

    Find stock ticker working on search page

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -20,7 +20,7 @@
       <button type="submit" id="search" class="button">Search</button>
       </form>
 
-      <form @submit.prevent="findCompany" class="form u-margin-bottom">
+      <!-- <form @submit.prevent="findCompany" class="form u-margin-bottom">
         <div class="input-fields">
           <div class="form-group u-margin-bottom-small">
             <label for="search" class="label">Find a Company</label>
@@ -34,7 +34,33 @@
           </div>
         </div>
       <button type="submit" id="search" class="button">Search</button>
+      </form> -->
+
+
+  <form @submit.prevent="tickerSearch" class="form u-margin-bottom">
+        <div class="input-fields">
+          <div class="form-group u-margin-bottom-small">
+            <label for="search" class="label">Find a Ticker</label>
+            <input
+              type="text"
+              id="search"
+              v-model="companyFragment"
+              autofocus
+              required
+            />
+          </div>
+        </div>
+      <button type="submit" id="search" class="button">Search</button>
       </form>
+
+      <ul>
+        <li v-for="company in companySearch"
+          v-bind:key="company.symbol">
+          {{company.symbol}}
+          {{company.securityName}}
+        </li>
+      </ul>
+         
 
     {{quote.latestPrice}}
     {{company.symbol}}
@@ -76,7 +102,12 @@ export default {
             industry: '',
             website: '',
             description: '', 
-          }
+          },
+          companyFragment: '',
+          companySearch: [{
+            symbol: '',
+            securityName: ''
+          }]
 
         }
     },
@@ -102,6 +133,18 @@ export default {
           this.company = company;
         })
     },
+    tickerSearch(){
+      //Returns an array of symbols up to the top 10 matches
+    const companyFragment = this.companyFragment;
+      fetch(`https://cloud.iexapis.com/v1/search/${companyFragment}?token=${process.env.VUE_APP_API_KEY}`)
+        .then((response) => {
+          return response.json();
+        })
+        .then((companySearch) => {
+          this.companySearch = companySearch;
+        })
+
+    }
 
    },
   }
