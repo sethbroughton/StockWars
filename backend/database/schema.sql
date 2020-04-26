d22416cced1185666b1f30e98f394fc2f3c0133f
Seth Broughton <broughton.24@gmail.com>
2020-04-26 19:27:38 -0400

    Changed trade table stock value to decimal type

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -64,8 +64,8 @@ CREATE TABLE trade
         type varchar(64) not null,
         quantity int not null,
         date_of_purchase date not null,
-        stock_value money not null,
-        commission money not null,
+        stock_value decimal not null,
+        commission decimal not null,
         
         constraint pk_trade primary key (trade_id),
         constraint fk_trade_portfolio foreign key (portfolio_id) references portfolio (portfolio_id)
