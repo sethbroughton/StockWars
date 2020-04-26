57a5ec7c8d77db7dbdd412741caadc86359b78e4
Seth Broughton <broughton.24@gmail.com>
2020-04-25 21:35:41 -0400

    Updated database test sql script

--- backend/database/DatabaseTestScript.sql
+++ backend/database/DatabaseTestScript.sql
@@ -1,6 +1,6 @@
 --Users -- Password is "password" for both entries
-INSERT INTO users (username, password, salt, role) VALUES ('jake', 'Isv0ZInVPcF2ZDKcrhltSw==', 'Gid+gJga/PggeuFhxYsqBvRXStwRrw/qt22elyZQwbkPMMSo2bcbc8VE26f9JZOucXH96J+S8UW6I6Paxj3GwHIRSBBM/y7qIK+nIxIIKk6Rfdbx+rpDnWq8arwdxKuMo1R+/RhLnSwNraAm8hQPaGxzYQ1V7Sj6ZXZBY8mynpE=', user)
-INSERT INTO users (username, password, salt, role) VALUES ('kevin', 'EKF4ZepfTgVP/qjktyKhxQ==', '52N/YS8FilCKROxiKMfIpbb++4Fma9LLEmrESvwPl2zYcRfVoBey3n73hrtd2DtcNF/5he+LjFboc5IqfPKrqSWBZceQjFAbFxuWGFUnS/z7ZQqLUTIdkwOPuuDdf8s/aWa5/aBB/KDHYp0Gm/khQFq0MKiHbLjZ2qH8fgByrq0=', user)
+INSERT INTO users (username, password, salt, role) VALUES ('jake', 'Isv0ZInVPcF2ZDKcrhltSw==', 'Gid+gJga/PggeuFhxYsqBvRXStwRrw/qt22elyZQwbkPMMSo2bcbc8VE26f9JZOucXH96J+S8UW6I6Paxj3GwHIRSBBM/y7qIK+nIxIIKk6Rfdbx+rpDnWq8arwdxKuMo1R+/RhLnSwNraAm8hQPaGxzYQ1V7Sj6ZXZBY8mynpE=', user);
+INSERT INTO users (username, password, salt, role) VALUES ('kevin', 'EKF4ZepfTgVP/qjktyKhxQ==', '52N/YS8FilCKROxiKMfIpbb++4Fma9LLEmrESvwPl2zYcRfVoBey3n73hrtd2DtcNF/5he+LjFboc5IqfPKrqSWBZceQjFAbFxuWGFUnS/z7ZQqLUTIdkwOPuuDdf8s/aWa5/aBB/KDHYp0Gm/khQFq0MKiHbLjZ2qH8fgByrq0=', user);
 
 --Pending Games
 INSERT INTO game (organizer_id, name, number_of_players, length_in_days) VALUES (1, 'Game1', 7, 5);
@@ -16,9 +16,14 @@ UPDATE game SET start_date = '2020-04-18', end_date = '2020-04-28' WHERE game.ga
 INSERT INTO portfolio (user_id, game_id, total_value) VALUES (1, 1, 120000);
 INSERT INTO portfolio (user_id, game_id, total_value) VALUES (1, 2, 90000);
 INSERT INTO portfolio (user_id, game_id, total_value) VALUES (1, 3, 200000);
-INSERT INTO portfolio (user_id, game_id, total_value) VALUES (1, 4, -10000);
+INSERT INTO portfolio (user_id, game_id, total_value) VALUES (2, 1, 100000);
 
 --Trades - Buy
-INSERT INTO trade (portfolio_id, type, ticker, quantity, stock_value, commission, date_of_purchase) VALUES (1, 'BUY', 'AAPL', 100, 19, 0, '2020-04-25')
+INSERT INTO trade (portfolio_id, type, ticker, quantity, stock_value, commission, date_of_purchase) VALUES (1, 'BUY', 'AAPL', 100, 1000, 0, '2020-04-23');
+
+--Trades - Sell
+INSERT INTO trade (portfolio_id, type, ticker, quantity, stock_value, commission, date_of_purchase) VALUES (1, 'SELL', 'AAPL', 10, 110, 0, '2020-04-24')
+
+
 
 
