a49dcf0bb746c1a90112f03dd0e2ec97ef3ae1af
Jeff Crosley <jeffcrosley@gmail.com>
2020-04-30 22:49:04 -0400

    style tweaks

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -294,6 +294,6 @@ button:focus {
 .u-margin-bottom-small { margin-bottom: 1rem !important; }
 .u-margin-bottom { margin-bottom: 3rem !important; }
 .u-margin-bottom-large { margin-bottom: 5rem !important; }
-.u-margin-bottom-x-large { margin-bottom: 10rem !important; }
+.u-margin-bottom-x-large { margin-bottom: 8.5rem !important; }
 
 </style>
